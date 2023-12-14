f7029c7dc9310da6ced2178d118e3c6f5ebd572a
Nikita Samoilov <107534176+1RMAsik@users.noreply.github.com>
2023-12-14 14:15:23 +0300

    client.py

--- Lab18/client.py
+++ Lab18/client.py
@@ -5,24 +5,20 @@ from tkinter import scrolledtext
 
 def connect_to_server():
     global client_socket
-    # Создайте сокет
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    # Подключитесь к серверу
     client_socket.connect((address_entry.get(), int(port_entry.get())))
 
     client_status.insert(END, "Connected to server\n")
 
 def disconnect_from_server():
     global client_socket
-    # Отключитесь от сервера
     client_socket.close()
 
     client_status.insert(END, "Disconnected from server\n")
 
 def send_to_server():
     global client_socket
-    # Отправьте данные серверу
     client_socket.send(message_entry.get().encode())
 
     # Получите ответ от сервера
@@ -30,10 +26,8 @@ def send_to_server():
 
     client_status.insert(END, f"Sent: {message_entry.get()}\nReceived: {response}\n")
 
-# Создайте окно
 root = Tk()
 root.title("Nikita Samoilov 9PZ-56 Client")
-# Создайте элементы интерфейса
 address_label = Label(root, text="Address")
 address_entry = Entry(root)
 port_label = Label(root, text="Port")
@@ -45,7 +39,6 @@ message_entry = Entry(root)
 send_button = Button(root, text="Send", command=send_to_server)
 client_status = scrolledtext.ScrolledText(root)
 
-# Разместите элементы интерфейса на окне
 address_label.pack()
 address_entry.pack()
 port_label.pack()
@@ -57,5 +50,4 @@ message_entry.pack()
 send_button.pack()
 client_status.pack()
 
-# Запустите главный цикл обработки событий
 root.mainloop()
